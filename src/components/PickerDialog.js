bcf3d9b2057b295e1b85b40af00c684aaef18271
Jamie Woods <jamie.woods@bbc.co.uk>
2018-01-02 14:06:12 +0000

    Add support for differing colour pickers

--- src/components/PickerDialog.js
+++ src/components/PickerDialog.js
@@ -1,28 +1,37 @@
 
 import React, { PropTypes } from 'react'
-import { ChromePicker } from 'react-color'
+// import reactColor from 'react-color'
+let reactColor = require('react-color')
 
 const PickerDialog = ({
   value,
   onClick,
-  onChange
-}) => (
-  <div style={{ position: 'absolute', zIndex: '2' }}>
-    <div
-      style={{ position: 'fixed', top: '0px', right: '0px', bottom: '0px', left: '0px' }}
-      onClick={onClick}
-    />
-    <ChromePicker
-      color={value}
-      onChange={onChange}
-    />
-  </div>
-)
+  onChange,
+  pickerType,
+  disableAlpha
+}) => {
+  let PickerType = reactColor[(pickerType || 'Chrome') + 'Picker']
+  window.console.log((pickerType || 'Chrome') + 'Picker', reactColor, PickerType)
+  return (
+    <div style={{ position: 'fixed', zIndex: '2' }}>
+      <div
+        style={{ position: 'fixed', top: '0px', right: '0px', bottom: '0px', left: '0px' }}
+        onClick={onClick}
+      />
+      <PickerType
+        color={value}
+        disableAlpha={ disableAlpha || false}
+        onChange={onChange}
+      />
+    </div>
+  )
+}
 
 PickerDialog.propTypes = {
   value: PropTypes.string,
   onChange: PropTypes.func,
-  onClick: PropTypes.func
+  onClick: PropTypes.func,
+  disableAlpha: PropTypes.bool
 }
 
 export default PickerDialog
--- stories/index.js
+++ stories/index.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import { storiesOf, action } from '@kadira/storybook'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import Toolbar from 'material-ui/Toolbar'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
 injectTapEventPlugin()
@@ -11,10 +12,25 @@ import ColorPicker from '../src'
 storiesOf('ColorPicker', module)
   .add('simple', () => (
     <MuiThemeProvider>
+    <Toolbar style={{ background: 'none' }}>
       <ColorPicker
         defaultValue='#000'
         onChange={action('changed')}
-        floatingLabelText='Color picker'
+        floatingLabelText='Color Picker'
       />
+      <ColorPicker
+        defaultValue='#000'
+        onChange={action('changed')}
+        disableAlpha={ true }
+        floatingLabelText='No Alpha'
+      />
+      <ColorPicker
+        defaultValue='#000'
+        onChange={action('changed')}
+        disableAlpha={ true }
+        pickerType='Circle'
+        floatingLabelText='Circle Picker'
+      />
+    </Toolbar>
     </MuiThemeProvider>
   ))
