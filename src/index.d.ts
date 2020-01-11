8c436497f42fc6cf4af0481eea24173fb26c1a5a
Khartir <thomas.gnandt@mayflower.de>
2020-01-11 10:22:25 +0100

    fix types

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,15 +3,15 @@ import { converters } from "./transformers";
 import { FC } from "react";
 
 type props = {
-  defaultValue: string;
+  defaultValue?: string;
   onChange: (color: string) => void;
-  convert: keyof converters;
-  hintText: string;
-  floatingLabelText: string;
-  showPicker: boolean;
-  internalValue: string;
-  setShowPicker: (open: boolean) => void;
-  setValue: (value: string) => void;
+  convert?: keyof converters;
+  hintText?: string;
+  floatingLabelText?: string;
+  showPicker?: boolean;
+  internalValue?: string;
+  setShowPicker?: (open: boolean) => void;
+  setValue?: (value: string) => void;
 } & Omit<TextFieldProps, "onChange">;
 
 declare const ColorPicker: FC<props>;
