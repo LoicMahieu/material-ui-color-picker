87f9cbe198953629ab7ee3ddd403c142654cc0a9
Lior Chen <liorchen2@gmail.com>
2020-03-23 12:04:12 +0200

    wrap the absolute div in relative due to bug where the absolute container badly positioned

--- src/components/PickerDialog.js
+++ src/components/PickerDialog.js
@@ -7,15 +7,17 @@ const PickerDialog = ({
   onClick,
   onChange
 }) => (
-  <div style={{ position: 'absolute', zIndex: '2' }}>
-    <div
-      style={{ position: 'fixed', top: '0px', right: '0px', bottom: '0px', left: '0px' }}
-      onClick={onClick}
-    />
-    <ChromePicker
-      color={value}
-      onChange={onChange}
-    />
+  <div style={{ position: 'relative' }}>
+    <div style={{ position: 'absolute', zIndex: '2' }}>
+      <div
+        style={{ position: 'fixed', top: '0px', right: '0px', bottom: '0px', left: '0px' }}
+        onClick={onClick}
+      />
+      <ChromePicker
+        color={value}
+        onChange={onChange}
+      />
+    </div>
   </div>
 )
 
