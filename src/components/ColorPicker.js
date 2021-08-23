035cbeaa0ad1e0b2052488dfbf9a6b0d224fe1f2
Amir Sobhi <amir.sobhi@gmail.com>
2021-08-23 16:26:27 -0400

    using react 17

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import compose from 'recompose/compose'
+import compose from 'react-recompose/compose'
 import withState from 'recompose/withState'
 
 import TextField from '@material-ui/core/TextField'
